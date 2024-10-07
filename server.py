997429281a3330f341b5e5fcbeba180189b4ea63
jakeleesh <leeshaoherng@hotmail.com>
2024-10-07 13:19:09 -0400

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,10 @@
+"""
+This module is a Flask application for detecting emotions in text.
+
+It provides an endpoint `/emotionDetector` that accepts text input and
+returns the detected emotions along with their scores. The dominant emotion
+is also identified and returned.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +12,17 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emo_detector():
+    """
+    Handle requests to the /emotionDetector endpoint.
+
+    Retrieves the text to analyze from the request arguments, processes it
+    using the emotion_detector function, and returns the detected emotions
+    and their scores. If the input is invalid, an error message is returned.
+
+    Returns:
+        str: A formatted string with the detected emotions and their scores,
+             or an error message if the input is invalid.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_detect = request.args.get('textToAnalyze')
 
@@ -22,12 +40,22 @@ def emo_detector():
     # Check if the label is None, indicating an error or invalid input
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        # Return a formatted string with the emotion and score
-        return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+
+    # Return a formatted string with the emotion and score
+    return (
+    f"For the given statement, the system response is 'anger': {anger_score}, "
+    f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and "
+    f"'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+)
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the index page.
+
+    Returns:
+        str: The rendered HTML content for the index page.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
